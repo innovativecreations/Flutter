fc2492f11320265467d0cca677d00afc68242d19
Mayank <innovativecreations195@gmail.com>
2023-01-30 21:07:38 +0530

    change the text with button

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,16 +6,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var ques = [
+    "What's your fav food?",
+    "What's your fav animal?",
+  ];
+  int Qno = 0;
+
+  void changeQNo() {
+    print(ques.length);
+    setState(() {
+      if (Qno != ques.length - 1) {
+        Qno += 1;
+      } else {
+        Qno -= 1;
+      }
+    });
+
+    print(Qno);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Yo")),
-        body: Column(children: const [
-          Text("Hey, I've alive"),
+        body: Column(children: [
+          Text(ques[Qno]),
           ElevatedButton(
-            onPressed: null,
+            onPressed: changeQNo,
             child: Text('Button'),
           ),
         ]),
