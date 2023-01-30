f7837e1bb755f4127a132b8c13974699ce1352cc
Mayank <innovativecreations195@gmail.com>
2023-01-30 19:12:40 +0530

    just some text

---
+++ first_app/README.md
@@ -0,0 +1,5 @@
+# first_app
+My first app
+(actually not quiet the first app, but still it is )
+
+
---
+++ first_app/lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Yo")),
+        body: Text("Hey, I've alive"),
+      ),
+    );
+  }
+}
